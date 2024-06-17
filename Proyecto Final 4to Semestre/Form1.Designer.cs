0c76b020b195bf4d63770508035e597b23e2450c
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-06-17 13:59:56 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto Final 4to Semestre/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Proyecto_Final_4to_Semestre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGuardarCSV = new System.Windows.Forms.Button();
+            this.btnGuardarJSON = new System.Windows.Forms.Button();
+            this.btnGuardarXML = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1209, 519);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnGuardarCSV
+            // 
+            this.btnGuardarCSV.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnGuardarCSV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarCSV.Location = new System.Drawing.Point(54, 638);
+            this.btnGuardarCSV.Name = "btnGuardarCSV";
+            this.btnGuardarCSV.Size = new System.Drawing.Size(100, 28);
+            this.btnGuardarCSV.TabIndex = 1;
+            this.btnGuardarCSV.Text = "CSV";
+            this.btnGuardarCSV.UseVisualStyleBackColor = true;
+            this.btnGuardarCSV.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnGuardarJSON
+            // 
+            this.btnGuardarJSON.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnGuardarJSON.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarJSON.Location = new System.Drawing.Point(160, 638);
+            this.btnGuardarJSON.Name = "btnGuardarJSON";
+            this.btnGuardarJSON.Size = new System.Drawing.Size(100, 28);
+            this.btnGuardarJSON.TabIndex = 2;
+            this.btnGuardarJSON.Text = "JSON";
+            this.btnGuardarJSON.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardarXML
+            // 
+            this.btnGuardarXML.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnGuardarXML.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardarXML.Location = new System.Drawing.Point(266, 638);
+            this.btnGuardarXML.Name = "btnGuardarXML";
+            this.btnGuardarXML.Size = new System.Drawing.Size(100, 28);
+            this.btnGuardarXML.TabIndex = 3;
+            this.btnGuardarXML.Text = "XML";
+            this.btnGuardarXML.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(1055, 633);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "GUARDAR";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(730, 638);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 28);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "XML";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(624, 638);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 28);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "JSON";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(518, 638);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 28);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "CSV";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 559);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1207, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "_________________________________________________________________________________" +
+    "________________________________________________________________________________" +
+    "_______________________________________";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 593);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "GUARDAR EN FORMATOS:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(598, 594);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "LEER LOS FORMATOS:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(996, 594);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(219, 19);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "GUARDAR MODIFICACIONES:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1157, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(33, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1188, 8);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(33, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button9
+            // 
+            this.button9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button9.Location = new System.Drawing.Point(1122, 8);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(33, 23);
+            this.button9.TabIndex = 15;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1233, 689);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnGuardarXML);
+            this.Controls.Add(this.btnGuardarJSON);
+            this.Controls.Add(this.btnGuardarCSV);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGuardarCSV;
+        private System.Windows.Forms.Button btnGuardarJSON;
+        private System.Windows.Forms.Button btnGuardarXML;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Proyecto Final 4to Semestre/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.IO;
+
+namespace Proyecto_Final_4to_Semestre
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          LoadData();
+        }
+
+
+        #region Metodo para cargar los datos de la base de datos en el DataGridView
+        private void LoadData()
+        {
+            // Consulta SQL para obtener todos los registros de una tabla 
+            string query = "SELECT * FROM spotify_songs";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    // Crear un adaptador de datos
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+                    // Crear un DataTable para almacenar los datos
+                    DataTable dataTable = new DataTable();
+
+                    // Llenar el DataTable con los datos del adaptador
+                    dataAdapter.Fill(dataTable);
+
+                    // Asignar el DataTable como la fuente de datos del DataGridView
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    // Mostrar mensaje de error en caso de que ocurra alguna excepción
+                    MessageBox.Show("Error al cargar los datos: " + ex.Message);
+                }
+            }
+        }
+        #endregion
+
+        private void GuardarEnCSV(string rutaArchivo)
+        {
+            // Crear un objeto StreamWriter para escribir en el archivo CSV
+            using (StreamWriter writer = new StreamWriter(rutaArchivo))
+            {
+                // Escribir la primera fila con los encabezados de las columnas
+                StringBuilder filaEncabezados = new StringBuilder();
+                foreach (DataGridViewColumn columna in dataGridView1.Columns)
+                {
+                    filaEncabezados.Append(columna.HeaderText + ",");
+                }
+                writer.WriteLine(filaEncabezados.ToString());
+
+                // Recorrer las filas del DataGridView y escribirlas en el archivo CSV
+                foreach (DataGridViewRow fila in dataGridView1.Rows)
+                {
+                    StringBuilder filaDatos = new StringBuilder();
+                    foreach (DataGridViewCell celda in fila.Cells)
+                    {
+                        // Verificar si la celda está vacía antes de acceder a su valor
+                        if (celda.Value != null)
+                        {
+                            filaDatos.Append(celda.Value.ToString() + ",");
+                        }
+                        else
+                        {
+                            // Manejar celdas vacías (opcional)
+                            filaDatos.Append("[CELDA VACÍA],");
+                        }
+                    }
+                    writer.WriteLine(filaDatos.ToString());
+                }
+            }
+        }
+
+
+        #region BOTONES PARA GUARDAR EN FORMATO 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mostrar un diálogo para seleccionar la ruta del archivo CSV
+            SaveFileDialog dialogoGuardar = new SaveFileDialog();
+            dialogoGuardar.Filter = "Archivos CSV (*.csv)|*.csv";
+            dialogoGuardar.DefaultExt = "csv";
+
+            if (dialogoGuardar.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Guardar el contenido del DataGridView en el archivo seleccionado
+                    GuardarEnCSV(dialogoGuardar.FileName);
+
+                    // Mostrar un mensaje de confirmación
+                    MessageBox.Show("Datos guardados correctamente en el archivo: " + dialogoGuardar.FileName);
+                }
+                catch (Exception ex)
+                {
+                    // Manejar errores durante el proceso de guardado
+                    MessageBox.Show("Error al guardar el archivo: " + ex.Message);
+                }
+            }
+        }
+
+        #endregion
+
+        #region BOTONES PARA LEER LOS FORMATOS
+        private void button8_Click(object sender, EventArgs e)
+        {
+          
+        }
+        #endregion
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //Maximizar la pantalla y normalizarla
+            if (WindowState == FormWindowState.Normal)
+            {
+                WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //Minimizar la pantalla
+            WindowState = FormWindowState.Minimized;
+        }
+    }
+}
